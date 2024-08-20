83a7217f617d39a94dee4589ef0fef0120dc5b1a
Dawid Klos <klosdawid12@gmail.com>
2024-08-20 18:58:57 +0100

    add lock and sleep script to i3 setup

---
+++ dot_config/i3/executable_lock_and_sleep.sh
@@ -0,0 +1,49 @@
+##!/bin/bash
+#
+## Lock the screen
+#i3lock -i /home/dave/bg.png
+#
+## Sleep the laptop after 5 minutes (300 seconds)
+#sleep 300
+#pm-suspend
+
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ff00ffcc'
+TEXT='#ee00eeee'
+WRONG='#880000bb'
+VERIFYING='#bb00bbbb'
+
+i3lock \
+  --insidever-color=$CLEAR \
+  --ringver-color=$VERIFYING \
+  \
+  --insidewrong-color=$CLEAR \
+  --ringwrong-color=$WRONG \
+  \
+  --inside-color=$BLANK \
+  --ring-color=$DEFAULT \
+  --line-color=$BLANK \
+  --separator-color=$DEFAULT \
+  \
+  --verif-color=$TEXT \
+  --wrong-color=$TEXT \
+  --time-color=$TEXT \
+  --date-color=$TEXT \
+  --layout-color=$TEXT \
+  --keyhl-color=$WRONG \
+  --bshl-color=$WRONG \
+  \
+  --screen 1 \
+  --blur 5 \
+  --clock \
+  --indicator \
+  --time-str="%H:%M:%S" \
+  --date-str="%A, %Y-%m-%d" \
+  --keylayout 1
+
+sleep 300
+systemctl suspend
+systemctl hibernate
