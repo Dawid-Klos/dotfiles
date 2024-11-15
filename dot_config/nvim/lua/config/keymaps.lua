efa1ce6fcf1ea1ed8dafc1a29d094fbaff1f9d58
Dawid Klos <klosdawid12@gmail.com>
2024-11-15 21:39:08 +0000

    correct keymaps descriptions

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,15 +5,14 @@
 
 local map = vim.keymap.set
 
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move the highlighted text down", remap = true })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move the highlighted text up", remap = true })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move the highlighted text down", remap = true, silent = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move the highlighted text up", remap = true, silent = true })
 
 map("n", "<C-d>", "<C-d>zz", { desc = "Keep cursor in the middle", remap = true })
 map("n", "<C-u>", "<C-u>zz", { desc = "Keep cursor in the middle", remap = true })
 
-map("n", "n", "nzzzv", { desc = "Jump to the next line below", remap = true })
-map("n", "J", "mzJ`z", { desc = "Append current line at the end of above line", remap = true })
-map("n", "N", "Nzzzv", { desc = "Jump to the next line above", remap = true })
+map("n", "n", "nzzzv", { desc = "Jump to the next occurence of search", remap = true })
+map("n", "N", "Nzzzv", { desc = "Jump to the previous occurence of search", remap = true })
 
 -- greatest remap ever
 map("x", "<leader>p", [["_dP]], { desc = "Paste yanked text and keep yank", remap = true })
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
