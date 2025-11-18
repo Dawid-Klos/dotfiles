64616853a0028750a3cec51338d1144b93397bc4
Dawid Klos <klosdawid12@gmail.com>
2025-11-18 20:49:36 +0000

    nvim: update go debugger

--- dot_config/nvim/lua/plugins/nvim-dap-go.lua
+++ dot_config/nvim/lua/plugins/nvim-dap-go.lua
@@ -7,24 +7,31 @@ return {
     dap_go.setup(opts)
 
     local dap = require("dap")
-    dap.configurations.go = dap.configurations.go or {}
 
-    -- 1. Normal debug of current file (optional)
-    table.insert(dap.configurations.go, {
-      type = "go",
-      name = "Debug File",
-      request = "launch",
-      program = "${file}",
-    })
-
-    -- 2. Attach to Air process running with dlv headless
-    table.insert(dap.configurations.go, {
-      type = "go",
-      name = "Attach to Air",
-      request = "attach",
-      mode = "remote",
-      host = "127.0.0.1",
-      port = 2345,
-    })
+    -- 🧹 Define once (no duplicates)
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "Delve: Debug File",
+        request = "launch",
+        program = "${file}",
+        buildFlags = "-gcflags='all=-N -l'",
+      },
+      {
+        type = "go",
+        name = "Delve: Debug Package",
+        request = "launch",
+        program = "${workspaceFolder}",
+        buildFlags = "-gcflags='all=-N -l'",
+      },
+      {
+        type = "go",
+        name = "Delve: Attach to Air",
+        request = "attach",
+        mode = "remote",
+        host = "127.0.0.1",
+        port = 2345,
+      },
+    }
   end,
 }
