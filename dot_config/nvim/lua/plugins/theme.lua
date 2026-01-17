fd3323ddc85c5837c27a34257bc22c6417d568ed
Dawid Klos <klosdawid12@gmail.com>
2026-01-17 11:17:12 +0000

    nvim: add alternative theme

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -24,3 +24,18 @@ return {
     },
   },
 }
+-- lua/plugins/rose-pine.lua
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   opts = {
+--     transparent = true,
+--     styles = {
+--       sidebars = "transparent",
+--       floats = "transparent",
+--     },
+--   },
+--   config = function()
+--     vim.cmd("colorscheme rose-pine")
+--   end,
+-- }
