401e6867e4e02a822d22ee72452160e30e24659d
Dawid Klos <klosdawid12@gmail.com>
2026-02-12 20:51:25 +0000

    nvim: change theme

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -10,32 +10,32 @@
 --   },
 -- }
 -- # Transparent Catppuccin
-return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-  {
-    "catppuccin",
-    opts = {
-      transparent_background = true,
-    },
-  },
-}
--- lua/plugins/rose-pine.lua
 -- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
---   opts = {
---     transparent = true,
---     styles = {
---       sidebars = "transparent",
---       floats = "transparent",
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
 --     },
 --   },
---   config = function()
---     vim.cmd("colorscheme rose-pine")
---   end,
+--   {
+--     "catppuccin",
+--     opts = {
+--       transparent_background = true,
+--     },
+--   },
 -- }
+-- lua/plugins/rose-pine.lua
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+  config = function()
+    vim.cmd("colorscheme rose-pine")
+  end,
+}
